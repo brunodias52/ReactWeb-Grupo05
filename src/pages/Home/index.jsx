51235f55ef0d0ebb99fa4c5f136a76677f9a7ac6
brunodias52 <brunodias52@hotmail.com>
2021-11-24 16:29:25 -0300

    Adicionando o header, algumas páginas e atualizando rotas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "../../components/Header";
 
-import "../../../src/Global.css"
+import "../../Global.css"
 
 const MyHome = () => {
     return (
       <>
+      <div className=".components">
         <Header />
-        <h1>Meus produtos</h1>
-        </>
+        <h1>Home</h1>
+      </div>
+     </>
     )
 }
 
